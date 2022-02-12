d0286cdadd4e1a1b5e13ff9c599e54aedd06ae8b
Fahad Ali Khan <fk99970@gmail.com>
2022-02-12 12:43:53 +0500

    Authentication Implemented

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,23 +6,49 @@ const User = require('../models/User');
 // @route     POST /api/v1/auth/register
 // @access    Public
 exports.register = asyncHandler(async (req, res, next) => {
-  res.status(200).json({ success: true, message: 'registerUser' });
+  const { name, email, password, role } = req.body;
+  const user = await User.create({ name, email, password, role });
+  const token = user.getSignedJwtToken();
+  res.status(200).json({ success: true, token });
 });
 
 // @desc      Login user
 // @route     POST /api/v1/auth/login
 // @access    Public
 exports.login = asyncHandler(async (req, res, next) => {
-  res.status(200).json({ success: true, message: 'loginUser' });
+  const { email, password } = req.body;
+
+  // Validate emil & password
+  if (!email || !password) {
+    return next(new ErrorResponse('Please provide an email and password', 400));
+  }
+  // Check for user
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  // Check if password matches
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  // Create token
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({ success: true, token });
 });
 
 // @desc      Log user out / clear cookie
 // @route     GET /api/v1/auth/logout
-// @access    Publicnb
+// @access    Private
 exports.logout = asyncHandler(async (req, res, next) => {
   res.status(200).json({
     success: true,
-    message: 'logoutUser',
+    token: '',
   });
 });
 
@@ -30,8 +56,10 @@ exports.logout = asyncHandler(async (req, res, next) => {
 // @route     GET /api/v1/auth/me
 // @access    Private
 exports.getMe = asyncHandler(async (req, res, next) => {
+  // user is already available in req due to the protect middleware
+  const user = req.user;
   res.status(200).json({
     success: true,
-    message: 'getMe',
+    data: user,
   });
 });
